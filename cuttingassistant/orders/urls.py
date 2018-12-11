7b4b9aa7cd83279ea54a1aa0979ad1b4d46b61a8
Panos Tolios <panostolios@gmail.com>
2018-12-11 19:49:55 +0200

    Add order-details view

--- cuttingassistant/orders/urls.py
+++ cuttingassistant/orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import OrderListView
+from .views import OrderListView, OrderDetailsView
 
 urlpatterns = [
-    path('', OrderListView.as_view(), name='orders_list')
+    path('', OrderListView.as_view(), name='order_list'),
+    path('<int:pk>', OrderDetailsView.as_view(), name='order_details')
 ]
--- cuttingassistant/orders/views.py
+++ cuttingassistant/orders/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Order
 
@@ -6,4 +6,10 @@ from .models import Order
 class OrderListView(ListView):
     model = Order
     context_object_name = 'order_list'
-    template_name = 'order-list.html'
+    template_name = 'order_list.html'
+
+
+class OrderDetailsView(DetailView):
+    model = Order
+    context_object_name = 'order'
+    template_name = 'order_details.html'
