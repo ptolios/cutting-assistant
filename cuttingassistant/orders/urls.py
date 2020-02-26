d31e28c3b1711f47076442e9214f4f99c8eace95
Panos Tolios <panostolios@gmail.com>
2020-02-27 00:40:44 +0200

    Add url path for new order

--- cuttingassistant/orders/urls.py
+++ cuttingassistant/orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import OrderListView, OrderDetailsView
+from .views import OrderCreateView, OrderDetailsView, OrderListView
 
 urlpatterns = [
     path('', OrderListView.as_view(), name='order_list'),
-    path('<int:pk>', OrderDetailsView.as_view(), name='order_details')
+    path('<int:pk>', OrderDetailsView.as_view(), name='order_details'),
+    path('new', OrderCreateView.as_view(), name='order_new'),
 ]
