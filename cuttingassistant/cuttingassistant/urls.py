00334deedfc27cdb4804d21a9e21964b8bd65810
Panos Tolios <panostolios@gmail.com>
2020-02-29 21:30:56 +0000

    Temporarily redirect homepage to orders page

--- cuttingassistant/cuttingassistant/urls.py
+++ cuttingassistant/cuttingassistant/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Temporarily redirect to orders page
+    path('', RedirectView.as_view(url='orders/'), name='home'),
     path('orders/', include('orders.urls'), name='orders')
 ]
